b3754029e4823d932b124b29e52d4eaa36ed1e2a
Caitlyn G. <cqitlyng@gmail.com>
2024-09-16 19:14:46 -0500

    Code that will find the product of two numbers

---
+++ product.py
@@ -0,0 +1,5 @@
+firstNum = input("Enter your first number: ")
+firstNum = int(firstNum)
+secondNum = input("Enter your second number: ")
+secondNum = int(secondNum)
+print("The product of your two numbers is ",firstNum * secondNum)
